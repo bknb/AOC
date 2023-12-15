d476d0570564ebd8edd3a9f99f64203a27729db4
bknb <93841180+bknb@users.noreply.github.com>
2023-12-15 05:57:04 +0000

    update

--- solutions/23/15/solution.js
+++ solutions/23/15/solution.js
@@ -1,10 +1,23 @@
 const {sum} = require('../../../santasLittleHelper.js');
-const {...parser} = require('../../../parser.js');
 let input, log;
 
 function solve1(inp,l) {
   input = inp, log = l;
   return sum(...input.map(hashCode));
+}
+
+function solve2(inp,l) {
+  input = inp, log = l;
+  const boxes = Array(256).fill([]).map(()=>[]);
+  const ins = input.map(seq=>[...seq.match(/\w+/g),/=/.test(seq)]);
+  ins.forEach(([name,fl,eq])=> {
+    const box = boxes[hashCode(name)];
+    const index = box.findIndex(([n])=>n===name);
+    if (!eq) index!==-1&&box.splice(index,1);
+    else if (index===-1) box.push([name,fl]);
+    else box[index][1] = fl;
+  });
+  return focusingPower(boxes);
 }
 
 function hashCode(seq) {
@@ -17,23 +30,9 @@ function hashCode(seq) {
   return current;
 }
 
-function solve2(inp,l) {
-  input = inp, log = l;
-  const boxes = Array(256).fill([]).map(x=>[]);
-  const ins = input.map(seq=>[...seq.match(/\w+/g),/=/.test(seq)]);
-  ins.forEach(([name,fl,eq])=> {
-    const box = boxes[hashCode(name)];
-    const index = box.findIndex(([n])=>n===name);
-    if (!eq) index!==-1&&box.splice(index,1);
-    else if (index===-1) box.push([name,fl]);
-    else box[index][1] = fl;
-  });
-  return focusingPower(boxes);
-}
-
 function focusingPower(boxes) {
   return sum(...boxes.map((ls,i)=>
-    ls.length&&sum(...ls.map(([lbl,fl],j)=>(i+1)*(j+1)*fl))));
+    ls.length&&sum(...ls.map(([,fl],j)=>(i+1)*(j+1)*fl))));
 }
 
 function init(data,log) {
