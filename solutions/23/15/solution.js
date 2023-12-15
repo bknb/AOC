6e35b5b367b8fb5b66aa432454474e7661078a2f
bknb <93841180+bknb@users.noreply.github.com>
2023-12-15 05:55:02 +0000

    23 15 2

--- solutions/23/15/solution.js
+++ solutions/23/15/solution.js
@@ -4,10 +4,10 @@ let input, log;
 
 function solve1(inp,l) {
   input = inp, log = l;
-  return sum(...input.map(hash));
+  return sum(...input.map(hashCode));
 }
 
-function hash(seq) {
+function hashCode(seq) {
   let current = 0;
   for (let c of seq) {
     current += c.charCodeAt(0);
@@ -19,7 +19,21 @@ function hash(seq) {
 
 function solve2(inp,l) {
   input = inp, log = l;
-  return null;
+  const boxes = Array(256).fill([]).map(x=>[]);
+  const ins = input.map(seq=>[...seq.match(/\w+/g),/=/.test(seq)]);
+  ins.forEach(([name,fl,eq])=> {
+    const box = boxes[hashCode(name)];
+    const index = box.findIndex(([n])=>n===name);
+    if (!eq) index!==-1&&box.splice(index,1);
+    else if (index===-1) box.push([name,fl]);
+    else box[index][1] = fl;
+  });
+  return focusingPower(boxes);
+}
+
+function focusingPower(boxes) {
+  return sum(...boxes.map((ls,i)=>
+    ls.length&&sum(...ls.map(([lbl,fl],j)=>(i+1)*(j+1)*fl))));
 }
 
 function init(data,log) {
