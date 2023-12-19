d82ec74a8b8d198fc95a611dd10734384a61d75d
bknb <93841180+bknb@users.noreply.github.com>
2023-12-19 07:57:13 +0000

    update

--- solutions/23/18/solution.js
+++ solutions/23/18/solution.js
@@ -1,11 +1,9 @@
 const {sum} = require('../../../santasLittleHelper.js');
 const {lineWise,linify} = require('../../../parser.js');
-let input, log;
 
 const letterDir={R:0,D:1,L:2,U:3};
 
-function solve1(inp,l) {
-  input = inp, log = l;
+function solve1(input) {
   let y = 0;
   const values = input.map(([dir,len])=>
       [dir%2?0:len*(dir>>1?1:-1),
@@ -15,14 +13,12 @@ function solve1(inp,l) {
   return sum(values.map(([dx,y])=>y*dx))+peri+1;
 }
 
-function solve2(inp,l) {
-  input = inp, log = l;
-  input = input.map(([,,c])=>
-    [+c.slice(-1)[0],parseInt(c.slice(0,-1),16)])
-  return solve1(input,l);
+function solve2(input) {
+  return solve1(input.map(([,,c])=>
+    [+c.slice(-1)[0],parseInt(c.slice(0,-1),16)]));
 }
 
-function init(data,log) {
+function init(data) {
   return lineWise().match().map(([a,b,c])=>
     [letterDir[a],+b,c])(linify(data));
 }
