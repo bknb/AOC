b1d82de8cce73b7bffd90043149f9adc2f176bb0
Topi <93841180+bknb@users.noreply.github.com>
2024-12-02 08:15:38 +0000

    update

--- solutions/24/2/solution.js
+++ solutions/24/2/solution.js
@@ -1,21 +1,28 @@
-const { allBut, count } = require('../../../santasLittleHelper.js');
+const { allBut, count, rng } = require('../../../santasLittleHelper.js');
 const {linify, lineWise} = require('../../../parser.js');
+
 
 const isSafe = row => row.slice(1)
     .map((x,i)=>[row[i]>x,Math.abs(row[i]-x)])
     .every(([inc,delta],i,[[first]])=>
       inc===first&&delta>0&&delta<4);
 
+const isSafe2 = row => {
+  const diffs = row.slice(1).map((x,i)=>row[i]-x);
+  return diffs.every(x=>rng(1,4).includes(x))
+    || diffs.every(x=>rng(1,4).includes(-x)); 
+}
+
 const enrichSubs = row =>
     [row,...row.map((x,i,arr)=>allBut(arr,i))]
 
 function solve1(input) {
-  return count(input, isSafe);
+  return count(input, isSafe2);
 }
 
 function solve2(input) {
   return count(input.map(row =>
-    enrichSubs(row).some(isSafe)));
+    enrichSubs(row).some(isSafe2)));
 }
 
 function init(data) {
