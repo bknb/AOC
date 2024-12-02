fdff7cc5f609253d6173758577fcd547dd05933d
Topi <93841180+bknb@users.noreply.github.com>
2024-12-02 06:28:29 +0000

    update

--- solutions/24/2/solution.js
+++ solutions/24/2/solution.js
@@ -1,9 +1,8 @@
-const {...helper} = require('../../../santasLittleHelper.js');
-const {linify} = require('../../../parser.js');
+const {linify, lineWise} = require('../../../parser.js');
 let input, log;
 
 const isSafe = row => row.slice(1)
-    .map((x,i)=>log([row[i]>x?'i':(row[i]<x?'d':'e'),Math.abs(row[i]-x)]))
+    .map((x,i)=>log([row[i]>x,Math.abs(row[i]-x)]))
     .map(([inc,delta])=>log([inc,delta>0&&delta<4]))
     .every(([inc,inRange],i,arr)=>inc===arr[0][0]&&inRange);
 
@@ -16,13 +15,13 @@ function solve2(inp,l) {
   input = inp, log = l;
   return input.map(row=>
     [row,...row.map((x,i,arr)=>
-      arr.filter((x,j)=>j!==i))]
+      arr.filter((y,j)=>j!==i))]
     .some(isSafe))
     .filter(x=>x).length;
 }
 
 function init(data,log) {
-  return linify(data).map(line=>line.split(' ').map(x=>+x));
+  return lineWise().match().numberfy()(linify(data));
 }
 
 module.exports = {init, solve1, solve2}
