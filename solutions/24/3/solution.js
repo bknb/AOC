13a056842014659d4a361c1f06a4d9e8d3d9cb33
Topi <93841180+bknb@users.noreply.github.com>
2024-12-03 06:08:30 +0000

    update

--- solutions/24/3/solution.js
+++ solutions/24/3/solution.js
@@ -1,29 +1,23 @@
-const {...helper} = require('../../../santasLittleHelper.js');
-const {lineWise} = require('../../../parser.js');
 let input, log;
 
 function solve1(inp,l) {
   input = inp, log = l;
-  return input.match(/mul\(\d+,\d+\)/g)
-    .map(x=>x.match(/\d+/g).map(y=>+y))
+  return input.map(x=>x.match(/\d+/g))
+    .filter(x=>x)
     .reduce((c,[a,b])=>c+a*b,0);
 }
 
 function solve2(inp,l) {
   input = inp, log = l;
-  const inst = log(input.match(/mul\(\d+,\d+\)|do(n't)?\(\)/g)
-      .map(x=>x.startsWith('d')?/o\(/.test(x):x.match(/\d+/g)));
-  let take = true;
-  let result = 0;
-  for (let i = 0; i < inst.length; i++) {
-    if (typeof inst[i] === 'boolean') take = inst[i];
-    else if (take) result+=+inst[i][0]*+inst[i][1];
-  }
-  return result;
+  return input
+    .map(x=>x.startsWith('d')?/o\(/.test(x):x.match(/\d+/g))
+    .reduce(([r,t],x)=>
+      typeof x === 'boolean' ? [r,x] :
+      (t?[r+x[0]*x[1],t]:[r,t]), [0,true])[0];
 }
 
 function init(data,log) {
-  return data;
+  return data.match(/mul\(\d+,\d+\)|do(n't)?\(\)/g);
 }
 
 module.exports = {init, solve1, solve2}
